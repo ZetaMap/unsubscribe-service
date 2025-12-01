9a9dc7b9cab62aba78568dcbd89cc21cd46fc676
ZetaMap <zack.filmgaming@gmail.com>
2025-12-01 10:24:09 +0100

    fixed 404

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# unsubscribe-service
-A game where you play as a person trying to cancel their unnecessary subscriptions. Inspired from: https://youtu.be/keyk-liCo8I
+# Unsubscribe Service
+A game where you play as a person trying to cancel their unnecessary subscriptions. <br>
+Inspired from: https://youtu.be/keyk-liCo8I
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ContactComponent } from './game_1/account/contact';
 import { AccountComponent } from './game_1/account/account';
 import { SubscriptionContractComponent } from './game_1/subscription_contract/subscription_contract';
 import { UnsubscribePageComponent } from './game_1/subscription_contract/unsubscribe_page';
+
 // Ce fichier définie les routes de l'application, c'est a dire les differentes pages accessibles via un URL
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
